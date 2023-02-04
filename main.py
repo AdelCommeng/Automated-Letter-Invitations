d6cf22403259dfec084a158d9a4f3b0ed50de8c0
Adel Agha <120202042+AdelCommeng@users.noreply.github.com>
2023-02-04 17:45:17 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+list_of_names=[]
+with open("Input/Names/invited_names.txt") as name_file:
+    data_file=name_file.readlines()
+    for x in data_file:
+        list_of_names.append(x.strip())
+    print(list_of_names)
+for name in list_of_names:
+    with open(f"Input/Letters/starting_letter.txt",) as letter_file:
+        content=letter_file.read()
+        content=content.replace("[name]", name)
+    with open(f"Output/ReadyToSend/letter_for_{name}.txt","w") as output_file:
+        output_file.write(content)
